ee318ef19fa13a793c0b3605355553c04aa0bb45
Johannes Wesch <upygb@student.kit.edu>
2025-12-15 12:29:53 +0100

    feat: undo kvzip_press changes

--- kvpress/__init__.py
+++ kvpress/__init__.py
@@ -22,7 +22,7 @@ from kvpress.presses.key_rerotation_press import KeyRerotationPress
 from kvpress.presses.keydiff_press import KeyDiffPress
 from kvpress.presses.knorm_press import KnormPress
 from kvpress.presses.kvsummary_press import KVSummaryPress
-from kvpress.presses.kvpress_squared_press import KVPressSquaredPress
+from kvpress.presses.kvsquared_press import KVSquaredPress
 from kvpress.presses.kvzip_press import KVzipPress
 from kvpress.presses.lagkv_press import LagKVPress
 from kvpress.presses.leverage_press import LeverageScorePress
@@ -74,7 +74,7 @@ __all__ = [
     "KeyDiffPress",
     "KVSummaryPress",
     "KVzipPress",
-    "KVPressSquaredPress",
+    "KVSquaredPress",
     "DecodingPress",
     "PrefillDecodingPress",
     "ExpectedAttentionStatsPress",
