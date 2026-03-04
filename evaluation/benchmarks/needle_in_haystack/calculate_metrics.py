adf3a8af079a47b1bc8e5a42245a06491e188493
Johannes Wesch <upygb@student.kit.edu>
2026-03-04 13:30:52 +0100

    feat: adapt niah calculation

--- evaluation/benchmarks/needle_in_haystack/calculate_metrics.py
+++ evaluation/benchmarks/needle_in_haystack/calculate_metrics.py
@@ -7,9 +7,12 @@ from rouge import Rouge
 scorer = Rouge()
 
 
+GROUND_TRUTH = "eat a sandwich and sit in Dolores Park on a sunny day."
+
+
 def calculate_metrics(df: pd.DataFrame) -> list[dict]:
     scores = []
     for index, row in df.iterrows():
-        score = scorer.get_scores(row["needle"].strip(), row["predicted_answer"].strip())[0]
+        score = scorer.get_scores(row["predicted_answer"].strip(), GROUND_TRUTH)[0]
         scores.append(score)
     return scores
